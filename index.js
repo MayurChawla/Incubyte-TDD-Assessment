87ef43ea8e3233b9dbe2d4bd6d34a8c89f7eeb1f
Mayur L Chawla <mr.mayurchawla@gmail.com>
2024-12-08 12:32:37 +0530

    step 3 - handling multiple delimiters

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const stringCalculator = {
     add: (numbers) => {
-        if (numbers === "") {
-            return 0;
-        }
-        const parts = numbers.split(",");
+        if (numbers === "") return 0;
+        const delimiters = [',', '\n'];
+
+        let parts = [numbers];
+        delimiters.forEach(delimiter => {
+            parts = parts.map(part => part.split(delimiter));
+            parts = parts.flat();
+        });
         return parts.reduce((sum, num) => sum + parseInt(num), 0);
     }
 };

a266b923cfaf3b6493bcfb9c7341232b0517a0d5
Mayur L Chawla <mr.mayurchawla@gmail.com>
2024-12-08 12:41:47 +0530

    step 5 - negative numbers not allowed

--- test.js
+++ test.js
@@ -28,3 +28,8 @@ test('Add - Handles new line as a delimiter', () => {
 test('Add - Support different delimiters', () => {
     expect(stringCalculator.add("//;\n1;2")).toBe(3);
 });
+
+//case 5
+test('Add - Throws error for negative numbers', () => {
+    expect(() => stringCalculator.add("1,-2,3,-3")).toThrow("Negatives not allowed: -2");
+});

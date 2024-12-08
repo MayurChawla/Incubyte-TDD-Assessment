1d6cd5f391e1dbaba3959f4390814e1ac5f15da2
Mayur L Chawla <mr.mayurchawla@gmail.com>
2024-12-08 12:11:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const stringCalculator = {
+    add: (numbers) => {
+        if (numbers === "") {
+            return 0;
+        }
+        return 0;
+    }
+};
+
+module.exports = stringCalculator;
